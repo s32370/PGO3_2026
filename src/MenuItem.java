9c5e7dbfb7af5585c702b29cb0a3152e449bc1b3
Herman <s32370@pjwstk.edu.pl>
2026-03-24 12:50:31 +0100

    adding MenuItem class with provided fields, begining work on getters and setters

---
+++ src/MenuItem.java
@@ -0,0 +1,32 @@
+public class MenuItem {
+
+    //a constructor,
+    private String code;
+    private String name;
+    private double price;
+    private String category;
+    private static int productCount = 0;
+//getters,
+
+        public {
+            this.code = code;
+            this.name = name;
+            this.price = price;
+            this.category = category;
+            productCount++;
+        }
+    //a static method returning the number of created products,
+
+
+    @Override
+    public String toString() {
+//an overridden toString(),
+
+    }
+
+    @Override
+    public String equals(){
+           // /an overridden equals() comparing products by code.
+    }
+
+}
