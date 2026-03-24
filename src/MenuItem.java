a83a30d30d568a6e330cd15303ac08e80e831840
Herman <s32370@pjwstk.edu.pl>
2026-03-24 12:54:31 +0100

    static methof getPorductCount and rest

--- src/MenuItem.java
+++ src/MenuItem.java
@@ -1,22 +1,24 @@
 public class MenuItem {
 
-    //a constructor,
     private String code;
     private String name;
     private double price;
     private String category;
     private static int productCount = 0;
-//getters,
 
-        public {
+        public MenuItem (String code, String name, double price, String category){
             this.code = code;
             this.name = name;
             this.price = price;
             this.category = category;
             productCount++;
-        }
+}
     //a static method returning the number of created products,
 
+
+    public static int getProductCount() {
+        return productCount;
+    }
 
     @Override
     public String toString() {
