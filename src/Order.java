d56260a2630cce41238bfb9b12c0683025693ae7
Herman <s32370@pjwstk.edu.pl>
2026-03-24 13:09:37 +0100

    class order added

---
+++ src/Order.java
@@ -0,0 +1,15 @@
+public class Order {
+
+    private int orderNumber
+    private CafeCustomer customer
+    private ArrayList<MenuItem> items
+    private boolean paid
+    private static int nextOrderNumber
+
+            //addItem(MenuItem item)
+    //calculateTotal()
+    //countItems()
+    //markAsPaid()
+    //public static int getNextOrderNumber() or another static method related to order numbering,
+    //an overridden toString().
+}
