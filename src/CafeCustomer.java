fc2b66d1a75ada93e7ddcf72f38d8b4ede92aa04
Herman <s32370@pjwstk.edu.pl>
2026-03-24 13:08:28 +0100

    cafecustomer class added

--- src/CafeCustomer.java
+++ src/CafeCustomer.java
@@ -1,8 +1,32 @@
 public class CafeCustomer {
 
-    private int customerId
-    private String firstName
-    private String lastName
-    private String email
+    private int customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public CafeCustomer(int customerId, String firstName, String lastName, String email) {
+        this.customerId = customerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+//a method returning full customer data
+    public static getCustomerData(){
+        System.out.println(customerId);
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(email);
+    }
+
+    @Override
+    public String toString() {
+
+    }
+
+    @Override
+    public String equals(){
+
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-//github setup
 
 public class Main {
     public static void main(String[] args) {
